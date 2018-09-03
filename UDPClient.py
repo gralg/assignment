2a740f13a9c0d8d20643beb676a07851198b3164
gralg <j345274@gmail.com>
2018-09-04 00:01:36 +0800

    Update UDPClient.py

--- UDPClient.py
+++ UDPClient.py
@@ -1,5 +1,5 @@
 from socket import *
-serverName = '127.0.0.1'
+serverName = '127.0.0.1'    //When i try to use the ip address on another computer,the codes doesn't work.Dunnp why.
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 message = input('Input lowercase sentence:')
